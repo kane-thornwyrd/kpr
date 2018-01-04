8b256657a669e2d750b5f25ba3bae7911344d255
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2018-01-04 06:03:22 +0100

    chore(CICD): add codecov, remove auto publish

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 set -e
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, not releasing"
-  exit 0
-fi
-
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  echo "We are releasing !"
-  npm run semantic-release
-fi
+echo "SUCCESS !!!"
--- scripts/test.sh
+++ scripts/test.sh
@@ -11,4 +11,4 @@ if [ "$TEST_DEBUG" ]; then
    node="node --inspect --debug-brk"
 fi
 
-$node node_modules/.bin/istanbul cover -x *.test.js -- node_modules/.bin/_mocha `scripts/_get-test-directories.sh` --opts tests/mocha.opts --grep "$TEST_GREP"
+$node node_modules/.bin/istanbul cover -x *.test.js --report lcovonly -- node_modules/.bin/_mocha `scripts/_get-test-directories.sh` --opts tests/mocha.opts && node_modules/.bin/codecov
